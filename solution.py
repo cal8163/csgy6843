8be22f4457d70d0a78aa5e1012d87eed871ccc23
cal8163 <78501227+cal8163@users.noreply.github.com>
2021-02-24 17:40:05 -0500

    Add files via upload

--- solution.py
+++ solution.py
@@ -57,8 +57,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send QUIT command and get server response.
     # Fill in start
-    quitCommand = "\r\nQUIT"
-    clientSocket.send(quitCommand.encode())
+    clientSocket.send('QUIT'.encode())
     recv5 = clientSocket.recv(1024).decode()
     #print(recv5)
     clientSocket.close()
